668bfe00474e31c524b301bddb671e473aa4fb3c
Munkhbilguun Munkhdemberel <moondiidev@gmail.com>
2024-06-30 15:29:41 +0800

    feat: Add styles.css and update HTML files to use it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,14 @@
-module.exports = { content: ["./src/**/*.{html,js}"], theme: {} };
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "custom-green-100": "#a0f0a0", // or your custom green color
+      },
+      scale: {
+        1: "1", // Define your custom scale if needed
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
