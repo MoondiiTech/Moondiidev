839acf565276a65c032d5c8a18f546e49fed017f
Munkhbilguun Munkhdemberel <moondiidev@gmail.com>
2024-07-15 18:52:09 +0800

    added thumbnails and play button to websites

--- app.js
+++ app.js
@@ -275,14 +275,6 @@ function initVideoControls() {
   videos.forEach((video) => {
     const loader = video.parentElement.querySelector(".loader");
 
-    if (isComputer) {
-      video.load();
-      video.removeAttribute("autoplay");
-    } else {
-      video.autoplay = true;
-      video.muted = true;
-    }
-
     // Show loader and hide video initially
     video.style.display = "none";
     loader.style.display = "block";
